b97d9ccc30342efec5cf7940c1f95bd9afc298a7
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2022-12-29 20:11:46 -0300

    Demot - Init

---
+++ docs/js/bootstrap.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * @description Bootstrap
+ *
+ * @author C. M. de Picciotto <cmdepicciotto@gmail.com>
+ *
+ */
+import AirImageBuilder from '../../src/app/effect-air/builder/air-image-builder.js';
+import Image2Pixel     from '../../src/image2pixel.js';
+
+export default class Bootstrap {
+    /**
+     *
+     * Run
+     *
+     * @param {{src: String, width: Number, height: Number}} image
+     * @param {String}                                       canvasId
+     * @param {String}                                       backgroundColor
+     *
+     * @returns {void}
+     *
+     */
+    run(image, canvasId, backgroundColor) {
+        new Image2Pixel(this.#createImageBuilder(image, canvasId, backgroundColor));
+    }
+
+    /**
+     *
+     * Factory method. Create image builder
+     *
+     * @param {{src: String, width: Number, height: Number}} image
+     * @param {String}                                       canvasId
+     * @param {String}                                       backgroundColor
+     *
+     * @returns ImageBuilder
+     *
+     */
+    #createImageBuilder(image, canvasId, backgroundColor) {
+        return new AirImageBuilder(
+            image,
+            canvasId,
+            null,
+            backgroundColor
+        );
+    }
+}
+
