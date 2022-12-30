1c670de3edcc9b14e212fa40814a8ff03adef97a
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2022-12-30 10:04:02 -0300

    Add air grayscale image builder class & air invert image builder class

---
+++ src/app/effect-air/builder/image-builder/air-image-builder/air-grayscale-image-builder.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @description Air grayscale image builder
+ *
+ * @author C. M. de Picciotto <cmdepicciotto@gmail.com>
+ *
+ */
+import AirImageBuilder            from '../air-image-builder.js';
+import AirGrayscaleImageProcessor from '../../../processor/image-processor/grayscale-image-processor/air-grayscale-image-processor.js';
+
+export default class AirGrayscaleImageBuilder extends AirImageBuilder {
+    /**
+     *
+     * Factory method. Create image processor
+     *
+     * @returns {AirGrayscaleImageProcessor}
+     *
+     * @protected
+     *
+     * @note This air effect does not need a full resolution image, that is way the gap is set to 3
+     *
+     */
+    _createImageProcessor() {
+        return new AirGrayscaleImageProcessor(this.context, this.image, 3);
+    }
+}
---
+++ src/app/effect-air/builder/image-builder/air-image-builder/air-invert-image-builder.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @description Air invert image builder
+ *
+ * @author C. M. de Picciotto <cmdepicciotto@gmail.com>
+ *
+ */
+import AirImageBuilder         from '../air-image-builder.js';
+import AirInvertImageProcessor from '../../../processor/image-processor/invert-image-processor/air-invert-image-processor.js';
+
+export default class AirInvertImageBuilder extends AirImageBuilder {
+    /**
+     *
+     * Factory method. Create image processor
+     *
+     * @returns {AirInvertImageProcessor}
+     *
+     * @protected
+     *
+     * @note This air effect does not need a full resolution image, that is way the gap is set to 3
+     *
+     */
+    _createImageProcessor() {
+        return new AirInvertImageProcessor(this.context, this.image, 3);
+    }
+}
